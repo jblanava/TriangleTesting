f0bcf7e2c66b249cd79a55a69f370d28be9adb10
lloretalvaro <61242108+lloretalvaro@users.noreply.github.com>
2021-03-09 12:03:22 +0100

    Proyecto inicial

---
+++ Practica1_Triangulo/src/triangulo/TrianguloTest.java
@@ -0,0 +1,14 @@
+package triangulo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TrianguloTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
