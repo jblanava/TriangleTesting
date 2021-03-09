4e7f3345875858e693f72671ccff11090f87aeea
jblanava <0619836326@uma.es>
2021-03-09 12:09:01 +0100

    Triangulo.java primera version

--- Practica1_Triangulo/src/triangulo/Triangulo.java
+++ Practica1_Triangulo/src/triangulo/Triangulo.java
@@ -2,4 +2,29 @@ package triangulo;
 
 public class Triangulo {
 
+	private int p;
+	private int q;
+	private int r;
+	
+	public Triangulo(int p,int q,int r) {
+		 
+		boolean valido = r < p+q && q < p+r && p< q+r ;
+		boolean ladosPositivos = p >= 0 && q >= 0 && r>=0;
+		
+		this.p = p;
+		this.q = q;
+		this.r = r;
+	}
+	
+	public boolean esEscaleno() {
+		return p != q && p!= r && q!=r;
+	}
+	
+	public boolean esIsosceles() {
+		return (p == q && p != r)|| (p == r && p != q )| (q == r && q != p);
+	}
+	
+	public boolean esEquilatero() {
+		return p == q && p == r && q == r;
+	}
 }
